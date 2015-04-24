911e6739f43dc7633e61487c9c186a2606583cc8
huazai007 <347042860@qq.com.com>
2015-04-24 22:15:08 +0800

    day02

--- day02/11.py
+++ day02/11.py
@@ -1,9 +1,10 @@
-arr = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
-for j in range(1,len(arr)):
-        key = arr[j]
-        i = j-1
-        while i>=0 and arr[i] > key:
-                arr[i+1] = arr[i]
-                i = i-1
-        arr[i+1] =key
-print arr
+array = [14,3,21,2,2,3,4111,22,3333,4]
+for i in range(1, len(array)): 
+	key = array[i] 
+	print key
+	j = i - 1 
+	while j >= 0 and key < array[j]: 
+		array[j + 1] = array[j] 
+		array[j] = key 
+		j-=1
+print array
